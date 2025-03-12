285b8221c3b102a610c9771440fb450fbe590875
SVz <stephane.vuillermet@gmail.com>
2025-03-12 09:26:59 +0100

    Ajoutez des fichiers projet.

---
+++ ms-rd_Handler/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ms_rd_Handler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ms-rd:subscribe?url=100.108.20.39
+            //check if args is not empty
+            if (args.Length > 0)
+            {
+                //check if args contains ms-rd:subscribe
+                if (args[0].Contains("ms-rd:subscribe"))
+                {
+                    //get the url
+                    string url = args[0].Split('=')[1];
+                    //open the url in the browser
+                    //launch rdp client with the url
+                    System.Diagnostics.Process.Start("C:\\Windows\\System32\\mstsc.exe", "/v:" + url);
+                }
+                else
+                {
+                    //if args does not contain ms-rd:subscribe, show a message
+                    Console.WriteLine("Invalid arguments passed, must be like 'ms-rd:subscribe?url=100.108.20.39'");
+                }
+            }
+            else
+            {
+                //if args is empty, show a message
+                Console.WriteLine("No arguments passed");
+            }
+        }
+    }
+}
---
+++ ms-rd_Handler/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("ms-rd_Handler")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hopital de Royan")]
+[assembly: AssemblyProduct("ms-rd_Handler")]
+[assembly: AssemblyCopyright("Copyright © Hopital de Royan 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// L'affectation de la valeur false à ComVisible rend les types invisibles dans cet assembly
+// aux composants COM. Si vous devez accéder à un type dans cet assembly à partir de
+// COM, affectez la valeur true à l'attribut ComVisible sur ce type.
+[assembly: ComVisible(false)]
+
+// Le GUID suivant est pour l'ID de la typelib si ce projet est exposé à COM
+[assembly: Guid("7d3e20b2-bf80-46da-984f-191a880914c9")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire
+//      Numéro de build
+//      Révision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
