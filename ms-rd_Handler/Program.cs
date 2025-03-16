6b106ebbf9ebf928909fc0762e4dd09242f14c75
SVz <stephane.vuillermet@gmail.com>
2025-03-16 10:16:31 +0100

    Registry comment added

--- ms-rd_Handler/Program.cs
+++ ms-rd_Handler/Program.cs
@@ -36,4 +36,16 @@ namespace ms_rd_Handler
             }
         }
     }
+//    Windows Registry Editor Version 5.00
+
+//[HKEY_CLASSES_ROOT\ms-rd]
+//@="URL:Remote Desktop Protocol"
+//"URL Protocol"=""
+
+//[HKEY_CLASSES_ROOT\ms-rd\shell]
+
+//    [HKEY_CLASSES_ROOT\ms-rd\shell\open]
+
+//    [HKEY_CLASSES_ROOT\ms-rd\shell\open\command]
+//@="\"C:\\Users\\SVz\\source\\repos\\ms-rd_Handler\\ms-rd_Handler\\bin\\Release\\ms-rd_Handler.exe\" \"%1\""
 }
